faf89355d4b242eef0fade0019c087f2917c1451
Malou <malouhansen30@gmail.com>
2026-04-07 17:39:20 +0200

    tilgængelige quiz

--- frontend/vue-project/src/components/UserPanel.vue
+++ frontend/vue-project/src/components/UserPanel.vue
@@ -1,7 +1,42 @@
 <script setup>
 import { ref } from "vue";
+import PanelTabs from "./PanelTabs.vue";
+import StatsComponent from "./StatsComponent.vue";
+import UserQuizzes from "./UserQuizzes.vue";
+import UserResults from "./UserResults.vue";
+
+const activeTab = ref("userQuizzes");
+const stats = [
+  {
+    label: "Quizzer taget",
+    value: 6,
+    icon: "📄",
+  },
+  {
+    label: "Gennemsnitlig Score",
+    value: "82%",
+    icon: "📊",
+  },
+  {
+    label: "Total tid",
+    value: 25,
+    icon: "👥",
+  },
+];
 </script>
 
-<template></template>
+<template>
+  <section class="user-panel">
+    <StatsComponent :stats="stats" />
+    <PanelTabs v-model="activeTab" />
 
-<style scoped></style>
+    <UserQuizzes v-if="activeTab === 'userQuizzes'" />
+    <UserResults v-else-if="activeTab === 'userResults'" />
+  </section>
+</template>
+
+<style scoped>
+.user-panel {
+  padding: 20px;
+}
+</style>
